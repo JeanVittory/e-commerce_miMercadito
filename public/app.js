64787b713a2dab31809c19fa6935230731b439f5
JeanVittory <jean.carjil@gmail.com>
2022-01-26 23:27:13 -0500

    actualizacion responsive, adelanto en el js

--- public/app.js
+++ public/app.js
@@ -19,8 +19,42 @@ let productosComprados = {
 };
 
 
-// Función que retorna el total de la cantidad comprada por el cliente:
+// Función que aumentara .5 cada vez que el cliente de click en el '+' de la tarjeta en cada producto calculado en KG
+let contadorKg = 0;
+const sumaKilogramos = () =>{
+    return contadorKg += 0.5
+}
+console.log(sumaKilogramos());
+console.log(sumaKilogramos());
+console.log(sumaKilogramos());
 
+
+// Función que aumentara .5 cada vez que el cliente de click en el '+' de la tarjeta en cada producto calculado en KG
+const restaKilogramos = () =>{
+    if(contadorKg === 0) return 0;
+    if(contadorKg > 0) return contadorKg -= 0.5;
+}
+console.log(restaKilogramos());
+
+
+// Función que aumentara de 1 en 1 cada vez que el cliente de click en el '+' de la tarjeta en cada producto calculado en unidades
+let contadorUnidades = 0;
+const SumaUnidades = () =>{
+    return contadorUnidades += 1;
+}
+console.log(SumaUnidades());
+
+
+// Función que disminuira de 1 en 1 cada vez que el cliente de click en el '-' de la tarjeta en cada producto calculado en unidades
+const RestaUnidades = () =>{
+
+    if(contadorUnidades > 0 ) return contadorUnidades -= 1;
+    if(contadorUnidades === 0) return 0;
+}
+console.log(RestaUnidades());
+
+
+// Función que retorna el total de la cantidad comprada por el cliente:
 const cantidadTotal = ()=>{
     const sumaCantidades = Object.values(productosComprados).reduce((acc, {cantidad}) => acc + cantidad , 0);
     return sumaCantidades;
@@ -29,7 +63,6 @@ console.log(cantidadTotal());
 
 
 //Función que retorna el subtotal de lo comprado por el cliente:
-
 const subTotal = () =>{
     const sumaTotal = Object.values(productosComprados).reduce((acc, {precio, cantidad}) => acc + precio*cantidad, 0);
     return sumaTotal.toFixed(3);
@@ -37,13 +70,15 @@ const subTotal = () =>{
 
 console.log(subTotal());
 
+
 // Esta funcion retorna el impuesto expresado en pesos el cual será sumado al gran total
 const ivaCalculado = ()=>{
     const resultadoIva = (subTotal() * 19) / 100;
     return resultadoIva;
 }
 
-console.log(ivaCalculado())
+console.log(ivaCalculado());
+
 
 //Funcion que retorna el total despues del impuesto de IVA (19% col)
 const granTotal = ()=>{
