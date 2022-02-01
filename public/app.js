4f36e69837079a0bbfbcbb9c5e5ebbc7a2a438e2
JeanVittory <jean.carjil@gmail.com>
2022-01-31 21:48:56 -0500

    Uso de propiedades del objeto Array

--- public/app.js
+++ public/app.js
@@ -1,22 +1,38 @@
 //Este objeto esta enfocado a ser dinámico en funcion de lo que el cliente vaya agregando desde el DOM
-let productosComprados = {
-    1:{
+let productosComprados = [
+    {
         nombre: 'Costilla de Res',
         precio: 9.600,
         //Este valor deberia ser introducido por un data-attribute que aumenta o disminuye según las unidades o kg comprados (0.5kg = 1, 1kg = 2, 1.5kg = 3 etc; 1und = 1 etc)
         cantidad: 1 
     },
-    2:{
+    {
         nombre: 'Queso Campesino',
         precio: 8.000,
         cantidad: 2
     },
-    3:{
+    {
         nombre: 'Arroz Flor Huila',
         precio: 2.400,
         cantidad: 5
     }
+];
+
+const productoNuevo = prompt("Ingresa un nuevo producto: ");
+alert("Ese producto tiene un precio de $13.000 el kilo")
+const precioNuevo = 13.000;
+const cantidadNueva = parseInt(prompt("¿Cuantos kilos quieres comprar?: "));
+
+
+let objetoProductoNuevo = {
+    nombre: productoNuevo,
+    precio: precioNuevo,
+    cantidad: cantidadNueva
 };
+
+productosComprados.push(objetoProductoNuevo)
+
+console.log(productosComprados)
 
 
 // Función que aumentara .5 cada vez que el cliente de click en el '+' de la tarjeta en cada producto calculado en KG
@@ -24,8 +40,7 @@ let contadorKg = 0;
 const sumaKilogramos = () =>{
     return contadorKg += 0.5
 }
-console.log(sumaKilogramos());
-console.log(sumaKilogramos());
+
 console.log(sumaKilogramos());
 
 
@@ -34,6 +49,7 @@ const restaKilogramos = () =>{
     if(contadorKg === 0) return 0;
     if(contadorKg > 0) return contadorKg -= 0.5;
 }
+
 console.log(restaKilogramos());
 
 
@@ -56,7 +72,7 @@ console.log(RestaUnidades());
 
 // Función que retorna el total de la cantidad comprada por el cliente:
 const cantidadTotal = ()=>{
-    const sumaCantidades = Object.values(productosComprados).reduce((acc, {cantidad}) => acc + cantidad , 0);
+    const sumaCantidades = productosComprados.reduce((acc, {cantidad}) => acc + cantidad , 0);
     return sumaCantidades;
 };
 console.log(cantidadTotal());
@@ -64,7 +80,7 @@ console.log(cantidadTotal());
 
 //Función que retorna el subtotal de lo comprado por el cliente:
 const subTotal = () =>{
-    const sumaTotal = Object.values(productosComprados).reduce((acc, {precio, cantidad}) => acc + precio*cantidad, 0);
+    const sumaTotal = productosComprados.reduce((acc, {precio, cantidad}) => acc + precio*cantidad, 0);
     return sumaTotal.toFixed(3);
 };
 
@@ -89,3 +105,10 @@ const granTotal = ()=>{
 console.log(granTotal());
 
 
+const respuesta= confirm(`Tienes en tu carrito de compras ${productosComprados.length} productos listos para ser despachados! el total de tu compra sera de  $${granTotal()} ¿deseas confirmar tu orden?`);
+
+if(respuesta){
+    alert(`Muy bien! hemos despachado tu producto.`)
+}else{
+    alert("No hay problema! estaremos listos para despacharlo cuando lo desees!")
+}
