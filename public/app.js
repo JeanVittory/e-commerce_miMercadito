8b7d92160b35238c44359705b5813138f363e27f
JeanVittory <jean.carjil@gmail.com>
2022-02-07 23:08:12 -0500

    fetch de tarjetas

--- public/app.js
+++ public/app.js
@@ -1,54 +1,57 @@
+const cards = document.getElementById("cards");
+const templateCards = document.getElementById("template-cards").content;
+const fragment = document.createDocumentFragment();
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    fetchData();
+})
+
+const fetchData = async()=>{
+    try {
+        const res = await fetch('db.json');
+        const data = await res.json();
+        pintarCards(data);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const pintarCards = data =>{
+    data.forEach(element => {
+        templateCards.querySelector('img').setAttribute('src', element.imagen);
+        templateCards.querySelector('#dscto').textContent = element.Descuento;   
+        templateCards.querySelector('#titulo-card').textContent = element.Nombre;
+        templateCards.querySelector('#precio-antes').textContent = element.PrecioAntes.toFixed(3);
+        templateCards.querySelector('#precio-ahora').textContent = element.PrecioDespues.toFixed(3)
+        templateCards.querySelector('#unidades').textContent = element.UnidadDePeso;
+        const clone = templateCards.cloneNode(true)
+        fragment.appendChild(clone)
+    });
+
+    cards.appendChild(fragment)
+}
+
 //Este objeto esta enfocado a ser dinámico en funcion de lo que el cliente vaya agregando desde el DOM
-let productosComprados = [];
-
-let respuesta = true;
-
-while(respuesta){
-    
-    if(respuesta){
-
-        const productoNuevo = prompt("Ingresa un nuevo producto: ");
-        alert("Ese producto tiene un precio de $13.000 el kilo")
-        const precioNuevo = 13.000;
-        const cantidadNueva = parseInt(prompt("¿Cuantos kilos quieres comprar?: "));
-    
-    
-        let objetoProductoNuevo = {
-            nombre: productoNuevo,
-            precio: precioNuevo,
-            cantidad: cantidadNueva
-        };
-    
-        productosComprados.push(objetoProductoNuevo)
-    
-        console.log(productosComprados)
-    
+let productosComprados = [];    
+      
     
         // Función que aumentara .5 cada vez que el cliente de click en el '+' de la tarjeta en cada producto calculado en KG
         let contadorKg = 0;
         const sumaKilogramos = () =>{
             return contadorKg += 0.5
-        }
-    
-        console.log(sumaKilogramos());
-    
+        }    
     
         // Función que aumentara .5 cada vez que el cliente de click en el '+' de la tarjeta en cada producto calculado en KG
         const restaKilogramos = () =>{
             if(contadorKg === 0) return 0;
             if(contadorKg > 0) return contadorKg -= 0.5;
-        }
-    
-        console.log(restaKilogramos());
-    
+        }    
     
         // Función que aumentara de 1 en 1 cada vez que el cliente de click en el '+' de la tarjeta en cada producto calculado en unidades
         let contadorUnidades = 0;
         const SumaUnidades = () =>{
             return contadorUnidades += 1;
-        }
-        console.log(SumaUnidades());
-    
+        }    
     
         // Función que disminuira de 1 en 1 cada vez que el cliente de click en el '-' de la tarjeta en cada producto calculado en unidades
         const RestaUnidades = () =>{
@@ -56,34 +59,24 @@ while(respuesta){
             if(contadorUnidades > 0 ) return contadorUnidades -= 1;
             if(contadorUnidades === 0) return 0;
         }
-        console.log(RestaUnidades());
-    
     
         // Función que retorna el total de la cantidad comprada por el cliente:
         const cantidadTotal = ()=>{
             const sumaCantidades = productosComprados.reduce((acc, {cantidad}) => acc + cantidad , 0);
             return sumaCantidades;
         };
-        console.log(cantidadTotal());
-    
     
         //Función que retorna el subtotal de lo comprado por el cliente:
         const subTotal = () =>{
             const sumaTotal = productosComprados.reduce((acc, {precio, cantidad}) => acc + precio*cantidad, 0);
             return sumaTotal.toFixed(3);
-        };
-    
-        console.log(subTotal());
-    
+        };    
     
         // Esta funcion retorna el impuesto expresado en pesos el cual será sumado al gran total
         const ivaCalculado = ()=>{
             const resultadoIva = (subTotal() * 19) / 100;
             return resultadoIva.toFixed(3);
-        }
-    
-        console.log(ivaCalculado());
-    
+        }    
     
         //Funcion que retorna el total despues del impuesto de IVA (19% col)
         const granTotal = ()=>{
@@ -91,14 +84,7 @@ while(respuesta){
             return granTotal.toFixed(3);
         }
     
-        console.log(granTotal());
     
     
-        respuesta= confirm(`Tienes en tu carrito de compras ${productosComprados.length} productos listos para ser despachados! el total de tu compra sera de  $${granTotal()} ¿deseas comprar algo más?`);
-    }
 
-}
 
-if(!respuesta){
-    alert(`Muy bien! hemos despachado tu producto. Te agradecemos por la compra, vuelve pronto!`)
-}
