7a9aed27e8951275dac95a43c86bad41534cac3a
JeanVittory <jean.carjil@gmail.com>
2022-02-11 19:56:52 -0500

    filtro finalizado, sweetAlert seteado, inicio de creacion de array de productos comprados

--- public/app.js
+++ public/app.js
@@ -1,9 +1,18 @@
 const cards = document.getElementById("cards");
+const cantidadProducto = document.getElementById("cantidadProducto");
+let pseudoNumber =  document.getElementById("basketNumber");
 const templateCards = document.getElementById("template-cards").content;
 const fragment = document.createDocumentFragment();
+//Este array esta enfocado a ser dinámico en funcion de lo que el cliente vaya agregando desde el DOM
+let productosComprados = [];    
+    
 
 document.addEventListener("DOMContentLoaded", ()=>{
     fetchData();
+    cards.addEventListener("click", e =>{
+        addCarrito(e);
+        numberCarrito();
+    })
 }) 
 
 //Función que solicita informacion del db.json
@@ -18,7 +27,7 @@ const fetchData = async()=>{
         restarCantidadesUnd();
         filterProducts();
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
@@ -28,23 +37,25 @@ const pintarCards = data =>{
     data.forEach(element => {
         templateCards.querySelector('article').dataset.grupoFiltro = element.Grupo;        
         templateCards.querySelector('img').setAttribute('src', element.imagen);
-        templateCards.querySelector('img').setAttribute('alt', "producto")
+        templateCards.querySelector('img').setAttribute('alt', "producto");
         templateCards.querySelector('#dscto').textContent = element.Descuento;   
         templateCards.querySelector('#titulo-card').textContent = element.Nombre;
         templateCards.querySelector('#precio-antes').textContent = element.PrecioAntes.toFixed(3);
-        templateCards.querySelector('#precio-ahora').textContent = element.PrecioDespues.toFixed(3)
+        templateCards.querySelector('#precio-ahora').textContent = element.PrecioDespues.toFixed(3);
         templateCards.querySelector('#unidades').textContent = element.UnidadDePeso;
         templateCards.querySelector('#btn-less').dataset.id = element.id;
         templateCards.querySelector('#btn-less').dataset.unidades = element.UnidadDePeso;
-        templateCards.querySelector("#cantidadProducto").dataset.quantity = `cantidad${element.id}`
+        templateCards.querySelector("#cantidadProducto").dataset.quantity = `cantidad${element.id}`;
         templateCards.querySelector('#btn-increased').dataset.id = element.id;
-        templateCards.querySelector('#btn-increased').dataset.unidades = element.UnidadDePeso
-        const clone = templateCards.cloneNode(true)
-        fragment.appendChild(clone)
+        templateCards.querySelector('#btn-increased').dataset.unidades = element.UnidadDePeso;
+        templateCards.querySelector('.btn-comprar').dataset.id = element.id
+        const clone = templateCards.cloneNode(true);
+        fragment.appendChild(clone);
     });
-    cards.appendChild(fragment)
+    cards.appendChild(fragment);
 }
 
+//Esta funcion filtra los productos segun los data attributes seteados en el html y en la linea 31
 const filterProducts = ()=>{
     const btnsFilter = document.querySelectorAll(".btn");
     const productos = document.querySelectorAll(".producto");
@@ -52,31 +63,21 @@ const filterProducts = ()=>{
     for( i= 0; i < btnsFilter.length; i++){
         btnsFilter[i].addEventListener("click", e =>{
             e.preventDefault();
-            const filter = e.target.dataset.filter
-            //console.log(filter)
+            const filter = e.target.dataset.filter;
             
             productos.forEach(producto=>{
                 if(filter === "todos"){
-                    
-                    producto.classList.add("mostrar") 
-                    //console.log(producto.classList)
-                    console.log(filter)
-
+                    producto.classList.add("mostrar"); 
                 }else{
                     if(producto.dataset.grupoFiltro !== filter){
-                        producto.classList.remove("mostrar") 
-                        producto.classList.add("hidden") 
-                        //console.log(producto.dataset.grupoFiltro)
-                        
+                        producto.classList.remove("mostrar"); 
+                        producto.classList.add("hidden"); 
                     }else{
-                        producto.classList.remove("mostrar") 
-                        producto.classList.remove("hidden") 
-                        //console.log("adios")
-                        
+                        producto.classList.remove("mostrar"); 
+                        producto.classList.remove("hidden"); 
                     }
                 }
             })
-        
         })
     }
 }
@@ -150,30 +151,62 @@ const restarCantidadesUnd = ()=>{
     })
 }
 
-//Este objeto esta enfocado a ser dinámico en funcion de lo que el cliente vaya agregando desde el DOM
-let productosComprados = [];    
-      
-    
-// Función que aumentara .5 cada vez que el cliente de click en el '+' de la tarjeta en cada producto calculado en KG
-    
-// Función que aumentara .5 cada vez que el cliente de click en el '+' de la tarjeta en cada producto calculado en KG
-const restaKilogramos = () =>{
-    if(contadorKg === 0) return 0;
-    if(contadorKg > 0) return contadorKg -= 0.5;
-}    
-    
-// Función que aumentara de 1 en 1 cada vez que el cliente de click en el '+' de la tarjeta en cada producto calculado en unidades
-let contadorUnidades = 0;
-const SumaUnidades = () =>{
-    return contadorUnidades += 1;
-}    
-    
-// Función que disminuira de 1 en 1 cada vez que el cliente de click en el '-' de la tarjeta en cada producto calculado en unidades
-const RestaUnidades = () =>{
-    if(contadorUnidades > 0 ) return contadorUnidades -= 1;
-    if(contadorUnidades === 0) return 0;
+//esta funcion captura el elemento html que sera usado como constructor del objeto en setCarrito
+const addCarrito = e =>{
+     if(e.target.classList.contains("btn-comprar")){
+        setCarrito(e.target.parentElement)
+     }
+     e.stopPropagation()
 }
-    
+
+//Esta función crea un objeto por cada producto comprado y lo agrega al array productoscomprados
+const setCarrito = productos =>{
+
+    //Aqui se valida de que el usuario no intente agregar un producto con cantidad 0
+    if(productos.querySelector('#cantidadProducto').textContent !==  "0"){
+        const producto = {
+            id: productos.querySelector('.btn-comprar').dataset.id,
+            nombre: productos.querySelector('h2').textContent,
+            precio: productos.querySelector('#precio-ahora').textContent,
+            cantidad: parseFloat(productos.querySelector('#cantidadProducto').textContent),
+            unidad: productos.querySelector('#unidades').textContent,
+            
+        }
+        //aqui se valida si el producto ya existe en el array productosComprados
+        if(productosComprados.hasOwnProperty(producto.id)){
+            producto.cantidad = parseFloat(productosComprados[producto.id].cantidad) + parseFloat(producto.cantidad) 
+        }
+        productos.querySelector('#cantidadProducto').textContent = 0;
+        //esta linea agrega el objeto al array productosComprados
+        productosComprados[producto.id] = {...producto}
+
+
+    }else{
+        //esta linea ejecuta la biblioteca sweetAlert en el caso de que el usuario intente agregar un producto con cantidad 0
+        swal({
+            title: "Hemos detectado un problema :c",
+            text: "No has seleccionado la cantidad que deseas comprar",
+            icon: "error",
+            button: "Entendido!",
+          });
+    }
+}
+
+// continuamos...
+numberCarrito = ()=>{
+
+    let parsePseudoNumber = parseInt(pseudoNumber.textContent)
+    let result = parsePseudoNumber + 1;
+    console.log(result)
+};
+
+
+
+
+
+
+
+
 // Función que retorna el total de la cantidad comprada por el cliente:
 const cantidadTotal = ()=>{
     const sumaCantidades = productosComprados.reduce((acc, {cantidad}) => acc + cantidad , 0);
